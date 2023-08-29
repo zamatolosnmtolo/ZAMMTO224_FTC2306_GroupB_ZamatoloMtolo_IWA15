e7b94783b819d7088ecc1cd3ce3b01977ffda62d
Zamatolo Mtolo <135616311+zamatolosnmtolo@users.noreply.github.com>
2023-08-29 14:52:13 +0200

    Update js

--- scripts.js
+++ scripts.js
@@ -4,27 +4,33 @@ const data = {
 		['second', [2, 6, 8, 4, 14, 12, 10]],
 		['third', [9, 3, 1]],
 	]
-}
+};
 
 // Only edit below
 
-const { first = 1 } = data.first || {}
-const { second = 1 } = data.second || {}
-const { third = 1 } = data.third || {}
+const first = data.lists[0][1];
+const second = data.lists[1][1];
+const third = data.lists[2][1];
 
-const result = []
+const result = [];
 
 const extractBiggest = () => {
-	if (first[-1] > second[-1]) {
-		return first
-	}
+  if (first.length > 0 || second.length > 0 || third.length > 0) {
+    const firstValue = first.length > 0 ? first[first.length - 1] : -Infinity;
+    const secondValue = second.length > 0 ? second[second.length - 1] : -Infinity;
+    const thirdValue = third.length > 0 ? third[third.length - 1] : -Infinity;
 
-	if (third[-1] < 1) {
-		return second
-	}
-	
-	return third
-}
+    if (firstValue >= secondValue && firstValue >= thirdValue) {
+      return first.pop();
+    } else if (secondValue >= firstValue && secondValue >= thirdValue) {
+      return second.pop();
+    } else {
+      return third.pop();
+    }
+  } else {
+    return undefined;
+  }
+};
 
 // Only edit above
 
